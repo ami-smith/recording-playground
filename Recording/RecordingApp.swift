4b6f64100c171e33a65f2e89a398abe9da72123d
ami-smith <111449179+ami-smith@users.noreply.github.com>
2023-06-07 18:58:22 -0600

    ititial commit

---
+++ Recording/RecordingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordingApp.swift
+//  Recording
+//
+//  Created by Ami Smith on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct RecordingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
